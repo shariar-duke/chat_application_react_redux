f56f1222b14cafad987cb934ad1c957bf191d1e4
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-09 13:48:31 +0600

    apiSlice created

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,8 +1,13 @@
+/* eslint-disable no-undef */
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 export const apiSlice = createApi({
     reducerPath:"api", 
     baseQuery:fetchBaseQuery({
         baseUrl:process.env.REACT_APP_API_URL,
+    }),
+    tagTypes:[],
+    endpoints:(bulder)=> ({
+
     })
 })
