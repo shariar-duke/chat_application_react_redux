9a62852ff75806472cf3729451cddad1ccb05f76
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-10 20:41:06 +0600

    implemnted in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,64 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
 import LoadingSpinner from "./components/ui/LoadingSpinner";
 import useAuthCheck from "./hooks/userAuthCheck";
 import Conversation from "./pages/Conversation";
 import Inbox from "./pages/Inbox";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+
 function App() {
-    const authChecked = useAuthCheck(); // Call the auth check hook
+  const authChecked = useAuthCheck(); // Call the auth check hook
 
-    if (!authChecked) {
-        // Show loading spinner until the authentication check is complete
-        return (
-            <div className="flex items-center justify-center h-screen">
-                <LoadingSpinner />
-            </div>
-        );
-    }
-
-    // Render the app after the auth check is done
+  if (!authChecked) {
+    // Show loading spinner until the authentication check is complete
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/inbox" element={
-                  <PrivateRoute>  <Conversation /> </PrivateRoute>
-                } />
-                <Route path="/inbox/:id" element={
-                  <PrivateRoute>
-                      <Inbox /> 
-                  </PrivateRoute>
-                } />
-            </Routes>
-        </Router>
+      <div className="flex items-center justify-center h-screen">
+        <LoadingSpinner />
+      </div>
     );
+  }
+
+  // Render the app after the auth check is done
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/inbox"
+          element={
+            <PrivateRoute>
+              <Conversation />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/inbox/:id"
+          element={
+            <PrivateRoute>
+              <Inbox />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
