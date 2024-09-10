854586e86618efd9893ce22cb469d63744db67e2
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-10 18:41:29 +0600

    App.jsx component loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import LoadingSpinner from "./components/ui/LoadingSpinner";
+import useAuthCheck from "./hooks/userAuthCheck";
 import Conversation from "./pages/Conversation";
 import Inbox from "./pages/Inbox";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/inbox" element={<Conversation />} />
-        <Route path="/inbox/:id" element={<Inbox />} />
-      </Routes>
-    </Router>
-  );
+    const authChecked = useAuthCheck(); // Call the auth check hook
+
+    if (!authChecked) {
+        // Show loading spinner until the authentication check is complete
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <LoadingSpinner />
+            </div>
+        );
+    }
+
+    // Render the app after the auth check is done
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/inbox" element={<Conversation />} />
+                <Route path="/inbox/:id" element={<Inbox />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
