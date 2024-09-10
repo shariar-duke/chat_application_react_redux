a883b146ef65bc2f27fd960d4a8b8354c7eb38e4
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-10 11:16:03 +0600

    auth slice craeted

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,14 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState ={
-
+const initialState = {
+    accessToken: undefined,
+    user: undefined,
 }
 
 const authSlice = createSlice({
-    name : "auth", 
+    name: "auth",
     initialState, 
-    reducers: {}
+    reducers: {
+        userLoggedIn: (state, action) => {
+            state.accessToken = action.payload.accessToken; // Set accessToken
+            state.user = action.payload.user;               // Set user
+        },
+        userLoggedOut: (state) => {
+            state.accessToken = undefined; // Clear accessToken
+            state.user = undefined;        // Clear user
+        },
+    }
 })
 
-export const {} = authSlice.actions;
-export default authSlice.reducer
+// Correct export of actions
+export const { userLoggedIn, userLoggedOut } = authSlice.actions;
+
+// Correct export of reducer
+export default authSlice.reducer;
