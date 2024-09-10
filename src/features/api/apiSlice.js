755da379b7946c169b6c80d338675d4e2ea8c786
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-10 09:28:36 +0600

    error fix in apiSlice.js

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,13 +1,13 @@
-/* eslint-disable no-undef */
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+ 
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-    reducerPath:"api", 
-    baseQuery:fetchBaseQuery({
-        baseUrl:process.env.REACT_APP_API_URL,
+    reducerPath: "api", 
+    baseQuery: fetchBaseQuery({
+        baseUrl: import.meta.env.VITE_API_URL,  // Correct Vite way to access env variables
     }),
-    tagTypes:[],
-    endpoints:(bulder)=> ({
-
+    tagTypes: [],
+    endpoints: (builder) => ({   
+        // Define your endpoints here
     })
-})
+});
