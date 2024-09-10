fbc302516f44222b86fd2a6d9ae9574e86b6862b
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-10 20:21:24 +0600

    privatRoute wrapped in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
 import LoadingSpinner from "./components/ui/LoadingSpinner";
 import useAuthCheck from "./hooks/userAuthCheck";
 import Conversation from "./pages/Conversation";
 import Inbox from "./pages/Inbox";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-
 function App() {
     const authChecked = useAuthCheck(); // Call the auth check hook
 
@@ -24,8 +24,14 @@ function App() {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/inbox" element={<Conversation />} />
-                <Route path="/inbox/:id" element={<Inbox />} />
+                <Route path="/inbox" element={
+                  <PrivateRoute>  <Conversation /> </PrivateRoute>
+                } />
+                <Route path="/inbox/:id" element={
+                  <PrivateRoute>
+                      <Inbox /> 
+                  </PrivateRoute>
+                } />
             </Routes>
         </Router>
     );
