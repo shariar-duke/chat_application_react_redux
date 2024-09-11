6acceff519d297f9b9a2d59edc52ed9d69ac6cb8
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-11 08:52:34 +0600

    logout feature implemented

--- src/components/inbox/Navigation.jsx
+++ src/components/inbox/Navigation.jsx
@@ -1,6 +1,13 @@
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-
+import { userLoggedOut } from "../../features/auth/authSlice";
 export default function Navigation() {
+    const dispatch = useDispatch()
+    const handleLogout =()=> 
+    {
+         dispatch(userLoggedOut())
+         localStorage.clear()
+    }
     return (
         <nav className="bg-violet-700 border-b border-general sticky top-0 z-40 shadow-lg">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -12,6 +19,7 @@ export default function Navigation() {
 
                     {/* Logout Button */}
                     <button
+                     onClick={handleLogout}
                         className="bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-900 transition"
                     >
                         Logout
