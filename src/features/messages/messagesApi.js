8731d5662ee3e2d2ba5f9a3a78aefefe53879716
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-11 16:38:06 +0600

    message api added

--- src/features/messages/messagesApi.js
+++ src/features/messages/messagesApi.js
@@ -1,7 +1,14 @@
 import { apiSlice } from "../api/apiSlice";
 
 export const messages = apiSlice.injectEndpoints({
-    endpoints:(builder) => {
-        /// endpoints here 
-    }
+    endpoints: (builder) => ({
+        getMessages: builder.query({
+          query: (id) =>
+            `/messages?conversationId=${id}&_sort=timestamp&_order=desc&_page=1&_limit=${
+              import.meta.env.VITE_MESSAGE_PER_PAGE
+            }`,
+        }),
+      }),
 })
+
+export const { useGetMessagesQuery } = messages;
