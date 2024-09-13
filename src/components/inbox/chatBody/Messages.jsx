b22a858bd68cfd419d70f96cc58ff50201103cbc
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-13 07:11:41 +0600

    Messages component

--- src/components/inbox/chatBody/Messages.jsx
+++ src/components/inbox/chatBody/Messages.jsx
@@ -1,28 +1,17 @@
+ 
+/* eslint-disable react/prop-types */
+
 import Message from "./Message";
 
-export default function Messages() {
-    return (
-        <div className="relative w-full h-[calc(100vh_-_197px)] p-6 overflow-y-auto flex flex-col-reverse">
-            <ul className="space-y-2">
-                <Message justify="start" message="Hjdjd" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-            </ul>
-        </div>
-    );
+export default function Messages({ messages = [] }) {
+  return (
+    <div className="relative w-full h-[calc(100vh_-_197px)] p-6 overflow-y-auto flex flex-col-reverse">
+      <ul className="space-y-2">
+        {messages.map((message) => {
+            const {message : lastMessage, id} = message || {}
+          return <Message key={id} justify="start" message={lastMessage}/>;
+        })}
+      </ul>
+    </div>
+  );
 }
