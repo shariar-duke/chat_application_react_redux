5e20170877ae640c52f041de7e29893f99385478
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-13 11:21:42 +0600

    messages fetched dynamically

--- src/components/inbox/chatBody/Messages.jsx
+++ src/components/inbox/chatBody/Messages.jsx
@@ -1,15 +1,22 @@
  
 /* eslint-disable react/prop-types */
 
+import { useSelector } from "react-redux";
 import Message from "./Message";
 
 export default function Messages({ messages = [] }) {
+    const {user} = useSelector(state => state.auth || {})
+    const {email} = user || {}
+
+    console.log("The logged is user email is", email)
   return (
     <div className="relative w-full h-[calc(100vh_-_197px)] p-6 overflow-y-auto flex flex-col-reverse">
       <ul className="space-y-2">
-        {messages.map((message) => {
-            const {message : lastMessage, id} = message || {}
-          return <Message key={id} justify="start" message={lastMessage}/>;
+        {messages.slice().sort((a,b)=>a.timestamp - b.timestamp).map((message) => {
+         const {message : lastMessage, id ,sender} = message || {}
+         console.log(message)
+         const justify = sender.email !== email ? "start" : "end";
+          return <Message key={id} justify={justify} message={lastMessage}/>;
         })}
       </ul>
     </div>
