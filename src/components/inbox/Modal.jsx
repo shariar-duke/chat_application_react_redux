b31b55a1183045c513d7bd3d774575f35e218ce9
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-16 12:11:28 +0600

    all type of user and email check

--- src/components/inbox/Modal.jsx
+++ src/components/inbox/Modal.jsx
@@ -1,7 +1,75 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { conversationsApi } from "../../features/conversations/conversationsApi";
+import { useGetUserQuery } from "../../features/users/usersApi";
+import isValidEmail from "../../utils/isValidEmail";
+
 export default function Modal({ open, control }) {
-    if (!open) return null; // Don't render if modal is not open
-  
-    return (
+  const [to, setTo] = useState("");
+  const [message, setMessage] = useState("");
+  const [userCheck, setUserCheck] = useState(false);
+  const { user: loggedInUser } = useSelector((state) => state.auth) || {};
+  const { email: myEmail } = loggedInUser || {};
+  const [responseError, setResponseError] = useState("");
+  const [conversation, setConversation] = useState(undefined)
+  const dispatch = useDispatch();
+
+  const {
+    data: participant,
+    isLoading,
+    error,
+  } = useGetUserQuery(to, { skip: !userCheck });
+  const debounceHandler = (fn, delay) => {
+    let timeoutId;
+    return (...args) => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        fn(...args);
+      }, delay);
+    };
+  };
+
+  useEffect(() => {
+    if (participant?.length > 0 && participant[0].email !== myEmail) {
+      // check conversation existence
+      dispatch(
+        conversationsApi.endpoints.getConversation.initiate({
+          userEmail: myEmail,
+          participantEmail: to,
+        })
+      )
+        .unwrap()
+        .then((data)=> {
+          console.log("The message data is", data)
+          setConversation(data)
+        })
+        .catch((err) => {
+          setResponseError("There was a problem");
+        });
+    }
+  }, [participant, dispatch, myEmail, to]);
+
+  const doSearch = (value) => {
+    if (isValidEmail(value)) {
+      // check user API
+
+     
+
+      setUserCheck(true);
+      setTo(value);
+    }
+  };
+
+  const handleSearch = debounceHandler(doSearch, 500);
+
+  const handleSubmit =(e)=> 
+  {
+    e.preventDefault()
+    console.log("Form submitted")
+  }
+
+  return (
+    open && (
       <>
         <div
           onClick={control}
@@ -11,8 +79,7 @@ export default function Modal({ open, control }) {
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Send message
           </h2>
-          <form className="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" value="true" />
+          <form onSubmit={handleSubmit} className="mt-8 space-y-6">
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
                 <label htmlFor="to" className="sr-only">
@@ -21,10 +88,11 @@ export default function Modal({ open, control }) {
                 <input
                   id="to"
                   name="to"
-                  type="text"
+                  type="email"
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                   placeholder="Send to"
+                  onChange={(e) => handleSearch(e.target.value)}
                 />
               </div>
               <div>
@@ -34,24 +102,51 @@ export default function Modal({ open, control }) {
                 <textarea
                   id="message"
                   name="message"
+                  type="text"
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                   placeholder="Message"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
                 />
               </div>
             </div>
-  
+
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+                className={`group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 ${
+                  participant?.length === 0
+                    ? "cursor-not-allowed bg-gray-400 text-gray-700"
+                    : "cursor-pointer text-white bg-violet-600 hover:bg-violet-700 focus:ring-violet-500"
+                }`}
+                disabled={conversation == undefined || (participant?.length > 0 && participant[0].email === myEmail)}
               >
                 Send Message
               </button>
+              {participant?.length === 0 && (
+                <div className="mt-[10px] flex justify-center items-center text-red-400 font-bold">
+                  <p>This user does not exitsts</p>
+                </div>
+              )}
+
+              {participant?.length > 0 && participant[0].email === myEmail && (
+                <div className="mt-[10px] flex justify-center items-center text-red-400 font-bold">
+                  <p>You can not send message to yourself</p>
+                </div>
+              )}
+
+              {responseError && (
+                <div className="mt-[10px] flex justify-center items-center text-red-400 font-bold">
+                  <p>{responseError}</p>
+                </div>
+              )}
             </div>
+
+            {/* <Error message="There was an error" /> */}
           </form>
         </div>
       </>
-    );
-  }
-  
+    )
+  );
+}
--- src/components/inbox/chatBody/Messages.jsx
+++ src/components/inbox/chatBody/Messages.jsx
@@ -8,13 +8,11 @@ export default function Messages({ messages = [] }) {
     const {user} = useSelector(state => state.auth || {})
     const {email} = user || {}
 
-    console.log("The logged is user email is", email)
   return (
     <div className="relative w-full h-[calc(100vh_-_197px)] p-6 overflow-y-auto flex flex-col-reverse">
       <ul className="space-y-2">
         {messages.slice().sort((a,b)=>a.timestamp - b.timestamp).map((message) => {
          const {message : lastMessage, id ,sender} = message || {}
-         console.log(message)
          const justify = sender.email !== email ? "start" : "end";
           return <Message key={id} justify={justify} message={lastMessage}/>;
         })}
