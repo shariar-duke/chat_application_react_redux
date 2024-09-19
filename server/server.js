952a3f7050ff1ce8bf5b0c8f5bac1e4416c7586b
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-19 10:07:29 +0600

    socket installed in backend

--- server/server.js
+++ server/server.js
@@ -1,31 +1,31 @@
-const cors = require('cors');
 const auth = require("json-server-auth");
 const jsonServer = require("json-server");
+const express = require("express");
+const http = require("http");
 
-const server = jsonServer.create();
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+global.io = io;
+
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
 
 // Bind the router db to the app
-server.db = router.db;
+app.db = router.db;
 
-server.use(middlewares);
+app.use(middlewares);
 
-// Use CORS middleware
-server.use(cors());
-
-// Authentication rules
 const rules = auth.rewriter({
     users: 640,
     conversations: 660,
     messages: 660,
 });
 
-server.use(rules);
-server.use(auth);
-server.use(router);
+app.use(rules);
+app.use(auth);
+app.use(router);
 
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+server.listen(port);
